57450e3277d2b43e1017b7e419a8d7f769ebc76f
Sean <sean@parkross.com>
2025-04-14 14:56:17 +0100

    AnnouncementBar

--- src/theme/AnnouncementBar/index.tsx
+++ src/theme/AnnouncementBar/index.tsx
@@ -1,31 +1,41 @@
-import React, {type ReactNode} from 'react';
-import {useThemeConfig} from '@docusaurus/theme-common';
-import {useAnnouncementBar} from '@docusaurus/theme-common/internal';
-import AnnouncementBarCloseButton from '@theme/AnnouncementBar/CloseButton';
+import React, { type ReactNode, useEffect, useState } from 'react';
+import { useThemeConfig } from '@docusaurus/theme-common';
+import { useAnnouncementBar } from '@docusaurus/theme-common/internal';
+import { useColorMode } from '@docusaurus/theme-common';
 import AnnouncementBarContent from '@theme/AnnouncementBar/Content';
+import AnnouncementBarCloseBtn from '@site/static/icons/x-close.svg';
+import AnnouncementBarCloseBtnDark from '@site/static/icons/x-close-dark.svg';
 
 import styles from './styles.module.css';
 
 export default function AnnouncementBar(): ReactNode {
-  const {announcementBar} = useThemeConfig();
-  const {isActive, close} = useAnnouncementBar();
+  const { announcementBar } = useThemeConfig();
+  const { isActive, close } = useAnnouncementBar();
+  const { colorMode } = useColorMode();
+  const [definedColorMode, setDefinedColorMode] = useState('');
+
+  useEffect(() => {
+    setDefinedColorMode(colorMode);
+  }, [colorMode]);
+
+  const isDarkMode = definedColorMode === 'dark';
+
   if (!isActive) {
     return null;
   }
-  const {backgroundColor, textColor, isCloseable} = announcementBar!;
+
+  const { backgroundColor, textColor, isCloseable } = announcementBar!;
+
   return (
-    <div
-      className={styles.announcementBar}
-      style={{backgroundColor, color: textColor}}
-      role="banner">
-      {isCloseable && <div className={styles.announcementBarPlaceholder} />}
-      <AnnouncementBarContent className={styles.announcementBarContent} />
-      {isCloseable && (
-        <AnnouncementBarCloseButton
-          onClick={close}
-          className={styles.announcementBarClose}
-        />
-      )}
+    <div className={styles.announcementWrapper}>
+      <div className={styles.announcementBar} style={{ backgroundColor, color: textColor }} role="banner">
+        {isCloseable && (
+          <div onClick={close} className={styles.announcementBarClose}>
+            {isDarkMode ? <AnnouncementBarCloseBtnDark /> : <AnnouncementBarCloseBtn />}
+          </div>
+        )}
+        <AnnouncementBarContent className={styles.announcementBarContent} />
+      </div>
     </div>
   );
 }
